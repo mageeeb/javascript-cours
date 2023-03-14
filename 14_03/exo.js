08ccdfd5a274d0084e698581a7d36da5131958df
Magib <112185392+mageeeb@users.noreply.github.com>
2023-03-14 16:43:27 +0100

    14_03

---
+++ 14_03/exo.js
@@ -0,0 +1,11 @@
+/*if(onclick=0){
+    onclick="this.textContent++">0;
+}*/
+
+function changerCouleur(element) {
+    element.style.color='red';
+
+    if(element.style.color==='red'){
+        element.style.color='green'
+    }
+}
